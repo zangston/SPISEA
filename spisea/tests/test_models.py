d175286889fa7544a1034a43b03a4fcf3bbfa654
Matt Hosek <mwhosek@ifa.hawaii.edu>
2022-10-29 17:04:23 -0700

    Quick test function making sure new Baraffe isos work

--- spisea/tests/test_models.py
+++ spisea/tests/test_models.py
@@ -1,4 +1,5 @@
 # Test functions for the different stellar evolution and atmosphere models
+import time
 import pdb
 
 def test_evolution_models():
@@ -159,5 +160,82 @@ def test_filters():
             
     print('get_obs_str pass')
     print('Filters done')
+
+    return
+
+def test_Baraffe15_update():
+    """
+    Make sure expanded age/mass ranges of Baraffe15
+    models are working
+    """
+    from spisea import synthetic as syn
+    from spisea import reddening, evolution, atmospheres
+    from spisea.imf import imf, multiplicity
+    import numpy as np
+    
+    # Define cluster parameters
+    logAge = 5.8
+    AKs = 2.4
+    distance = 4000
+    cluster_mass = 10**5.
+    mass_sampling=1
+
+    # Test filters
+    filt_list = ['nirc2,J', 'nirc2,Kp']
+
+    startTime = time.time()
+    
+    evo = evolution.Baraffe15()
+    atm_func = atmospheres.get_merged_atmosphere
+
+    red_law = reddening.RedLawNishiyama09()
+    
+    iso = syn.IsochronePhot(logAge, AKs, distance,
+                            evo_model=evo, atm_func=atm_func,
+                            red_law=red_law, filters=filt_list,
+                            mass_sampling=mass_sampling)
+
+    print('Constructed isochrone: %d seconds' % (time.time() - startTime))
+
+    # Now to create the cluster.
+    imf_mass_limits = np.array([0.01, 0.5, 1, np.inf])
+    imf_powers = np.array([-1.3, -2.3, -2.3])
+
+    ##########
+    # Start without multiplicity
+    ##########
+    my_imf1 = imf.IMF_broken_powerlaw(imf_mass_limits, imf_powers,
+                                      multiplicity=None)
+    print('Constructed IMF: %d seconds' % (time.time() - startTime))
+    
+    cluster1 = syn.ResolvedCluster(iso, my_imf1, cluster_mass)
+    clust1 = cluster1.star_systems
+    print('Constructed cluster: %d seconds' % (time.time() - startTime))
+
+    # Let's make sure M = 0.01 Msun stars are in table, and that it
+    # reaches ~1.4 Msun
+    assert np.isclose(0.01, np.min(clust1['mass']), 0.02)
+    assert np.isclose(1.4, np.max(clust1['mass']), 0.1)
+
+    ##########
+    # Test with multiplicity
+    ##########
+    multi = multiplicity.MultiplicityUnresolved()
+    my_imf2 = imf.IMF_broken_powerlaw(imf_mass_limits, imf_powers,
+                                      multiplicity=multi)
+    print('Constructed IMF with multiples: %d seconds' % (time.time() - startTime))
+    
+    cluster2 = syn.ResolvedCluster(iso, my_imf2, cluster_mass)
+    clust2 = cluster2.star_systems
+    print('Constructed cluster with multiples: %d seconds' % (time.time() - startTime))
+
+    assert len(clust2) > 0
+    assert len(cluster2.companions) > 0
+    assert np.sum(clust2['N_companions']) == len(cluster2.companions)
+
+    # Let's make sure M = 0.01 Msun stars are in table, and that it
+    # reaches ~1.4 Msun
+    assert np.isclose(0.01, np.min(clust2['mass']), 0.02)
+    assert np.isclose(1.4, np.max(clust2['mass']), 0.1)
 
     return
